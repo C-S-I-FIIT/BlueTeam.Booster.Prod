f7d8baf67a4f5448755a7814f6eef163aee9587d
MatusMakay <matus.makay@gmail.com>
2024-05-07 21:08:29 +0200

    fix: init script

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+nohup /home/mmakay/blueteam-boobster/BlueTeam.Booster.Prod/syslog-ng/src/monitor-script.sh > /dev/null 2>&1 &
+
 cd /home/mmakay/blueteam-boobster/BlueTeam.Booster.Prod/src/Bc.CyberSec.Detection.Booster.Api
 docker compose up -d --build
 
-cd /home/mmakay/blueteam-boobster/BlueTeam.Booster.Prod/syslog-ng/src
-./monitor-script.sh &
