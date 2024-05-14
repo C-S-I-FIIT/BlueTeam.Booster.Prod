0d4118d87bcce4e82cd69dc6110f0f6144529dac
MatusMakay <matus.makay@gmail.com>
2024-05-14 21:15:24 +0200

    fix: init.sh

--- syslog-ng/conf/init.sh
+++ syslog-ng/conf/init.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-$CUR_DIR = $(pwd)
+CUR_DIR=$(pwd)
 cd /etc/syslog-ng
-rm syslog-ng.conf 
-cp $CUR_DIR/syslog-ng.conf .
-cp $CUR_DIR/filter-cisco-devices.conf conf.d
-mkdir conf.d/uc-filters
+rm syslog-ng.conf
+cp "$CUR_DIR/syslog-ng.conf" .
+cp "$CUR_DIR/filter-cisco-devices.conf" conf.d/
+mkdir -p conf.d/uc-filters
 touch conf.d/uc-filters/uc-filters.conf
+
